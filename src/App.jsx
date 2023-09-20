840ed30b98d75974c62bd17802af2a69e729d4df
Ricardo Alexandre Costa <ricardozanete@gmail.com>
2023-09-19 23:18:28 -0300

    Estrutura inicial App.jsx, TodoForm.jsx, Todo.jsx, App.css

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import "./App.css";
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([
+
+    {
+      id: 1,
+      text: "Criar funcionalidade no sistema",
+      category: "Trabalho",
+      isCompleted: "false",
+    },
+    {
+      id: 2,
+      text: "Ir para academia",
+      category: "Pessoal",
+      isCompleted: "false",
+    },
+    {
+      id: 3,
+      text: "Estudar Programação",
+      category: "Estudos",
+      isCompleted: "false",
+    }
+
+
+
+  ])
+
+  return (
+          <div className="app">
+            <h1>Lista de Tarefas</h1>
+            <div className="todo-list">
+              {todos.map((todo) =>(
+                <Todo todo={todo} /> 
+              ))}
+            </div>
+            <TodoForm />
+          </div>
+  );
+}
+
+export default App
