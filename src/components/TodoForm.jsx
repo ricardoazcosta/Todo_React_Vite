0988d3e373b21f2cf3c7098bbb9ae3e3902c8e9d
Ricardo Alexandre Costa <ricardozanete@gmail.com>
2023-09-19 23:55:18 -0300

    Criaçao da funçao de add tarefa e limpar campos

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,31 +1,38 @@
-import {useState} from "react"
+import { useState } from "react"
 
 const TodoForm = () => {
 
-    const[ value, setValue] = useState ("");
-    const [ category, setCategory] = useState ("");
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Enviou Form!")
+
+        if (!value || !category) return
+
+        // adicionar todo(tarefa)
+        // Limpar os Campos
+        console.log(value, category);
     }
 
-  return (
-    <div className='todo-form'>
-        <h2>Criar Tarefa</h2>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Digite o Titulo" id="" />
-            <select>
-                <option value="">Selecione uma categoria</option>
-                <option value="Trabalho">Trabalho</option>
-                <option value="Pessoal">Pessoal</option>
-                <option value="Estudos">Estudos</option>
+    return (
+        <div className='todo-form'>
+            <h2>Criar Tarefa</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Digite o Titulo"
+                value={value}
+                onChange={(e) => setValue(e.target.value)} />
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="">Selecione uma categoria</option>
+                    <option value="Trabalho">Trabalho</option>
+                    <option value="Pessoal">Pessoal</option>
+                    <option value="Estudos">Estudos</option>
 
-            </select>
-            <button type="submit">Criar Tarefa</button>
-        </form>
-    </div>
-  )
+                </select>
+                <button type="submit">Criar Tarefa</button>
+            </form>
+        </div>
+    )
 }
 
 export default TodoForm;
